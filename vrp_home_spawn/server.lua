555ee461d84364b5d19e69d5430ff2af05d46a02
Hammer Master <chai5142xshot@gmail.com>
2018-01-13 17:50:32 +0700

    Update server.lua

--- vrp_home_spawn/server.lua
+++ vrp_home_spawn/server.lua
@@ -2,11 +2,12 @@ local Proxy = module("vrp", "lib/Proxy")
 vRP = Proxy.getInterface("vRP")
 
 -- unfreeze on spawn
-AddEventHandler("vRP:playerSpawn", function(user_id, source, first_spawn)
+AddEventHandler("vRP:playerSpawn", function(source, first_spawn)
 	if not first_spawn then
+		local user_id = vRP.getUserId(source)
 		local address = vRP.getUserAddress(user_id)
 	    if address then 
-		  SetTimeout(5000,function()
+		  SetTimeout(80000,function()
 		    vRP.accessHome(user_id, address.home, address.number)
 		  end)
 	    end
